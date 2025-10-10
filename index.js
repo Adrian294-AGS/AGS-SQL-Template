c837d5d8eaf60502ee31805a2bd7db99f218bc75
Adrian <stgoadrian294@gmail.com>
2025-10-10 22:12:02 +0800

    Ags-Lib

--- index.js
+++ index.js
@@ -1,73 +1,90 @@
 import { exec } from "child_process";
 import { db } from "./database/dbConnection.js";
+
 class AGS {
-  #table;
-  #user;
-  #password;
-  #host;
-  #database;
   #sqlOperation;
 
   constructor(aUser, aPassword, aHost, aDatabase) {
-    this.#user = aUser;
-    this.#password = aPassword;
-    this.#host = aHost;
-    this.#database = aDatabase;
-
-    this.#sqlOperation = db(this.#user, this.#password, this.#host, this.#database);
-
+    this.#sqlOperation = db(aUser, aPassword, aHost, aDatabase);
   }
 
   async select(columnName, tableName) {
     const sql = `SELECT ${columnName || "*"} FROM ${tableName}`;
-    const [result] = await this.#sqlOperation.query(sql);
-    return result;
+    try {
+      const [result] = await this.#sqlOperation.query(sql);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  selectWithId(yourIdName, columnName) {
-    const sql = `SELECT ${columnName || "*"} FROM ${this.#table} WHERE ${
+  async selectWithId(yourIdName, columnName, tableName, idValue) {
+    const sql = `SELECT ${columnName || "*"} FROM ${tableName} WHERE ${
       yourIdName || "Id"
-    } = ?`;
-    return sql;
+    } = ${idValue}`;
+    try {
+      const [result] = await this.#sqlOperation.query(sql);
+      return result[0];
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  insertInto(tableName) {
-    const sql = `INSERT INTO ${tableName || this.#table} SET ?`;
-    return sql;
+  async insertInto(tableName, objectValue) {
+    const sql = `INSERT INTO ${tableName} SET ?`;
+    try {
+      const [result] = await this.#sqlOperation.query(sql, [objectValue]);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  update(yourIdName, tableName, columnAndValue) {
-    const sql = `UPDATE ${tableName || this.#table} SET ${columnAndValue} WHERE ${
+  async update(yourIdName, tableName, objectValue, idValue) {
+    const sql = `UPDATE ${tableName} SET ? WHERE ${
       yourIdName || "Id"
-    } = ?`;
-    return sql;
+    } = ${idValue}`;
+    try {
+      const [result] = await this.#sqlOperation.query(sql, [objectValue]);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  delete(tableName, yourIdName) {
-    const sql = `DELETE FROM ${tableName || this.#table} WHERE ${
-      yourIdName || "Id"
-    } = ?`;
-    return sql;
+  async delete(tableName, yourIdName, idValue) {
+    const sql = `DELETE FROM ${tableName} WHERE ${yourIdName || "Id"} = ${idValue}`;
+    try {
+      const [result] = await this.#sqlOperation.query(sql);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  selectJoin(tbl_a, tbl_b, columnName, onCondition, yourIdName, joinType) {
+  async selectLeftAndRightJoin(tbl_a, tbl_b, columnName, onCondition, yourIdName, joinType, idValue) {
     const sql = `SELECT ${
       columnName || "*"
     } FROM ${tbl_a} ${joinType} ${tbl_b} ON ${onCondition} WHERE ${yourIdName} = ?`;
-    return sql;
+    try {
+      const [result] = await this.#sqlOperation.query(sql,[idValue]);
+      return result[0];
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
 export const getDadJokes = async () => {
   return new Promise((resolve, reject) => {
-   try {
-    exec("curl https://icanhazdadjoke.com", (error, stdout, stderr) => {
+    try {
+      exec("curl https://icanhazdadjoke.com", (error, stdout, stderr) => {
         return resolve(stdout);
-    });
-   } catch (error) {
-    return reject(error);
-   }
-  })
+      });
+    } catch (error) {
+      return reject(error);
+    }
+  });
 };
 
 export default AGS;
